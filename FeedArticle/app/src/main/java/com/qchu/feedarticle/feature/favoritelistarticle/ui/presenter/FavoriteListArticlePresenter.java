45c928523e61051fcf511b54aac4eaa5a3fbea3b
Quoc Dung Chu <quocdung.chu@gmail.com>
2015-10-07 21:29:40 +0200

    Add Presenter of favorite list article.
    Implement: favorite list article

---
+++ FeedArticle/app/src/main/java/com/qchu/feedarticle/feature/favoritelistarticle/ui/presenter/FavoriteListArticlePresenter.java
@@ -0,0 +1,20 @@
+package com.qchu.feedarticle.feature.favoritelistarticle.ui.presenter;
+
+import com.qchu.feedarticle.common.Presenter;
+
+/**
+ * Created by quocdungchu on 07/10/15.
+ */
+public class FavoriteListArticlePresenter extends Presenter {
+
+
+	@Override
+	protected void onCreate() {
+
+	}
+
+	@Override
+	protected void onDestroy() {
+
+	}
+}
