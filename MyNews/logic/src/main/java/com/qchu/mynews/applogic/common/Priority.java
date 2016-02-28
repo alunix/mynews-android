91b815351ce7b1a436ac53901a8e10eb22e096cd
Louis Chu <louis.chu@insign.fr>
2016-02-28 11:13:37 +0100

    Add priority

---
+++ MyNews/logic/src/main/java/com/qchu/mynews/applogic/common/Priority.java
@@ -0,0 +1,24 @@
+package com.qchu.mynews.applogic.common;
+
+import com.google.common.base.Predicate;
+
+/**
+ * Created by Quoc Dung Chu on 28/02/16.
+ */
+public enum Priority {
+  WHEN_EVER(0),
+  LOW(1),
+  MEDIUM(2),
+  HIGH(3),
+  IMMEDIATE(4);
+
+  private final int value;
+
+  Priority(int value){
+    this.value = value;
+  }
+
+  public int getValue() {
+    return value;
+  }
+}
